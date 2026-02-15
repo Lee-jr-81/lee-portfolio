a5a68d43e72ea9904ba37a4dc074d1ba3a497edc
Lee-jr-81 <129674277+Lee-jr-81@users.noreply.github.com>
2026-02-15 17:06:45 +0000

    setup folder structure

---
+++ app/(pages)/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <section className="section-site">
+      <div className="container-site">
+        <h1 className="text-3xl text-primary font-bold underline">
+          Hello world!
+        </h1>
+        <h1 className="text-3xl text-primary font-mono underline">
+          Hello world!
+        </h1>
+      </div>
+    </section>
+  );
+}
---
+++ app/loading.tsx
@@ -0,0 +1,8 @@
+// app/loading.tsx
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center min-h-screen">
+      <p className="text-sm text-muted animate-pulse">Loading...</p>
+    </div>
+  );
+}
